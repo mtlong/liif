9230f298708ce70423cbde9d7ef76c354ef940bc
Long Mai <mai.t.long88@gmail.com>
2021-06-15 16:37:57 -0700

    06-15-2021 -- 4:37PM

--- script_examplar_finetune_decoder.sh
+++ script_examplar_finetune_decoder.sh
@@ -1,6 +1,8 @@
 
 CONFIG=./configs/train-examplar/finetune_rdn-liif-encoder.yaml
 NAME=examplar_finetune
+INPUT_PATH_PREFIX=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X
+RESULT_PATH_PREFIX=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder
 GPU_ID=1
 
 # IMG=0802.png
@@ -15,79 +17,79 @@ GPU_ID=1
 IMG=0809
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0810
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0818
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0820
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0822
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0823
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0825
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0826
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0834
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
--- script_examplar_finetune_decoder_2.sh
+++ script_examplar_finetune_decoder_2.sh
@@ -1,95 +1,96 @@
 
 CONFIG=./configs/train-examplar/finetune_rdn-liif-encoder.yaml
 NAME=examplar_finetune
+INPUT_PATH_PREFIX=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X
+RESULT_PATH_PREFIX=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder
 GPU_ID=0
-
 
 
 IMG=0835
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0836
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0837
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0839
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0846
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0849
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0855
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0879
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0883
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
 
 
 IMG=0898
 python train_liif_examplar.py --config $CONFIG --name $NAME --tag $IMG.png --gpu $GPU_ID
 MODEL_PATH=./save/examplar_finetune_$IMG.png/epoch-last.pth
-INPUT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/4X/$IMG.png
-RESULT_PATH=/mnt/HDD1/home/mtlong/workspace/2021/Aggregative_Learning/Sand_Box/liif/data/samples/Results/test_examplar_finetune_decoder/$IMG.png
+INPUT_PATH=$INPUT_PATH_PREFIX/$IMG.png
+RESULT_PATH=$RESULT_PATH_PREFIX/$IMG.png
 
-python MTL_inspect_cross_scale.py --gpu $GPU --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
+python MTL_inspect_cross_scale.py --gpu $GPU_ID --model $MODEL_PATH --input_path $INPUT_PATH --gt_path $GT_PATH --result_path $RESULT_PATH --mode $MODE 
